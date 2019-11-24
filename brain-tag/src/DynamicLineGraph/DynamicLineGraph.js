6af0f0f94defc8e37c53dcd1956fcf11cef3a25a
Samarth Patel <samarth.patel.22@hotmail.com>
2019-11-24 15:44:01 -0500

    Minor computational change

--- brain-tag/src/DynamicLineGraph/DynamicLineGraph.js
+++ brain-tag/src/DynamicLineGraph/DynamicLineGraph.js
@@ -14,6 +14,7 @@ class DynamicLineChart extends Component {
 		this.socket = this.props.socket;
 		
 		this.initialTime = 0; // time of when first datapoint is received
+		this.dataToShow = this.props.secondsToShow * 1000; // amount of data to show
 	}
 
 	componentDidMount() {
@@ -23,7 +24,7 @@ class DynamicLineChart extends Component {
         // set interval for rendering
         setInterval(() => {
             this.chart.render();
-        }, 1000/this.props.refreshRate);
+        }, 1000 / this.props.refreshRate);
 	}
 
 	/**
@@ -32,14 +33,12 @@ class DynamicLineChart extends Component {
 	updateChart(data) {
 		// sets initialTime for the first data point
 		if (this.initialTime === 0) this.initialTime = new Date().getTime() / 1000;
-
-		// let seconds = new Date().getTime() / 1000;
 		
 		dps.push({x: data["time"] - this.initialTime, y: parseFloat(data[this.props.channel])});
 
 		// since we want to show last x seconds and streaming rate is 1000Hz,
         // the last x seconds have x * 1000 data points
-		if (dps.length > this.props.secondsToShow * 1000 ) {
+		if (dps.length > this.dataToShow ) {
 			dps.shift();
 		}
     }
