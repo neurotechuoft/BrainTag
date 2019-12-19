b02b5f35de60d5dee3c27eac5d776a3e144e0fd5
Dakota McInnis <dakotamcinnis1@gmail.com>
2019-12-19 17:08:41 -0500

    Moved Record.js from Utils to Presentation Layer

--- brain-tag/src/index.js
+++ brain-tag/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Presenter from './presenter/';
+import Presenter from './Presenter/';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Presenter/>, document.getElementById('root'));
