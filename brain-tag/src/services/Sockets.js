4554ca79dd34e9b1f583501b3234d19d3999eb2b
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2019-12-18 21:27:58 -0500

    /src/Services/: Added the basic functionality of the services class. This services class should be responsible for all socket interactions

---
+++ brain-tag/src/services/Sockets.js
@@ -0,0 +1,31 @@
+// Class to Initialize Socket Connections
+import io from "socket.io-client";
+
+export default class Sockets {
+    constructor() {
+        if (! Sockets.instance) {
+            const connectionOptions =  {
+                "force new connection" : true,
+                //avoid having user reconnect manually in order to prevent dead clients after a server restart
+                "reconnectionAttempts": "Infinity", 
+                //before connect_error and connect_timeout are emitted.
+                "timeout" : 10000,                  
+                "transports" : ["websocket"]
+            };
+
+            // Socket to connect to NeurostackClient (Temporarily using DummyServer)
+            this.eeg_socket = io('http://localhost:8005', connectionOptions)
+                .on("connect", () => {
+                    console.log("Connected to Real time EEG socket.");
+                });
+            // Socket to connect to FileServer
+            this.store_socket = io('http://localhost:8009', connectionOptions)
+                .on("connect", () => {
+                    console.log("Connected to Data Storage socket.");
+                });
+
+            Sockets.instance = this;
+        }
+        return Sockets.instance
+    }
+}
