74157d073338abed4eb7aafb310aa267f77987a3
Dakota McInnis <dakotamcinnis1@gmail.com>
2019-12-18 21:51:59 -0500

    Make Toggle use this.props.children for text

--- brain-tag/src/Views/Components/Buttons/Toggle.js
+++ brain-tag/src/Views/Components/Buttons/Toggle.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
+import {Button} from 'react-bootstrap';
 
 class Toggle extends Component {
 
     constructor(props) {
       super(props);
-      this.text = this.props.text;
       this.bg = this.props.bg;
       this.state = {isToggleOn: true};
       this.handleClick = this.handleClick.bind(this);
@@ -20,15 +19,15 @@ class Toggle extends Component {
   
     render() {
       if(this.state.isToggleOn){
-        return(<Button className = "ml-3" onClick={this.handleClick}> {this.text}
+        return(<Button className = "ml-3" onClick={this.handleClick}> {this.props.children}
         </Button>);
       }
       else{
         return(<Button className = "ml-3"onClick={this.handleClick} style={{ backgroundColor: this.bg}}>
-          {this.text}
+          {this.props.children}
         </Button>);
       }
     }
   }
-  
+
   export default Toggle;
--- brain-tag/src/Views/Components/Buttons/index.js
+++ brain-tag/src/Views/Components/Buttons/index.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
+import {ButtonToolbar} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Toggle from './Toggle';
 import './index.css';
 class Buttons extends Component{
 	render() {
 		return (
-		<div>
-			<div>
-				<Toggle text= "&#11044;" bg="#F74F56" ></Toggle>
-				<Toggle text={"Up"}bg = '#5EBA7D' ></Toggle>
-				<Toggle text={"Down"}bg = '#5EBA7D' >DOWN</Toggle>
-				<Toggle text={"LEFT"}bg = '#5EBA7D' >LEFT</Toggle>
-				<Toggle text={"RIGHT"}bg = '#5EBA7D' ></Toggle>
-			</div>
+		<div className="ButtonBar">
+			<Toggle bg="#F74F56" >&#11044;</Toggle>
+			<Toggle bg = '#5EBA7D' >UP</Toggle>
+			<Toggle bg = '#5EBA7D' >DOWN</Toggle>
+			<Toggle bg = '#5EBA7D' >LEFT</Toggle>
+			<Toggle bg = '#5EBA7D' >RIGHT</Toggle>
 		</div>
 		);
 	}
