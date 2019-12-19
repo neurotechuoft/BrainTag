ee33ad7728a8d920a221760abfcc1531a84c976d
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2019-12-19 13:00:08 -0500

    Populated the base logic for the presentation/logic layer. This currently replaces the contents from App (now deleted).

--- brain-tag/src/presenter/index.js
+++ brain-tag/src/presenter/index.js
@@ -1,14 +1,35 @@
 // Presenter class
 import React, {Component} from 'react';
-import App from "../App/App";
+import ChannelView from '../Views/Channel';
+import io from 'socket.io-client';
+
+const VIEWS = {
+    CHANNELS: "channels"
+}
 
 /**
  * Singleton Presenter in MVP Architecture.
  */
 class Presenter extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            page: VIEWS.CHANNELS
+        }
+    }
 
     render() {
-        return(<App />);
+        if(this.state.page === VIEWS.CHANNELS){
+            var socket = io('http://localhost:8005/')
+                .on("connect", () => {
+                    console.log("connected!");
+                });
+            
+            return (
+                <ChannelView socket={socket} />
+            );
+        }
+        return "";
     }
 
 }
