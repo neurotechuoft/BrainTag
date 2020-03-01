7b5d829797f03edfbe91b600b9f94c20f0d185ae
swOOsh <ayushk2020@gmail.com>
2020-03-01 16:15:38 -0500

    Added Toggle View

--- brain-tag/src/Views/Components/Channels/Row.js
+++ brain-tag/src/Views/Components/Channels/Row.js
@@ -4,33 +4,46 @@ import React from 'react';
 import './index.css';
 import RawData from './Plots/RawData';
 import PSD from './Plots/PSD';
-import PropTypes from 'prop-types';
 
-export default class Row extends React.PureComponent {
-    render() {  
-        return(
-            <div  className="row">
-                <div className="channelTitle"> 
-                    {this.props.channelName}
-                </div>
-                <div className="column"> 
-                    <RawData 
-                        addEEGHandler={this.props.addEEGHandler}
-                        secondsToShow={1} 
-                        refreshRate={60}
-                        channel={this.props.channelName}
-                    />
-                </div>
-                <div className="column"> 
-                    <PSD options={this.props.options} />
-                </div>
+class Row extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.isOpen= true;
+      }
+    
+    changeStatus(){
+        this.isOpen = !this.isOpen;
+    }
+
+    returnStatus(){
+        return this.isOpen ? '&#9660' : '&#9658';
+    }
+
+    returnVisibility(){
+        return this.isOpen ? 'visibility: visible' : 'visibility: hidden';
+    }
+
+    render(){
+    
+    return(
+        <div  className="row">
+            <div className="channelTitle" onClick={this.changeStatus()}> 
+                {this.returnStatus()+this.props.channelName}
             </div>
+            <div className="column" style={this.returnVisibility()}> 
+                <RawData
+                    addEEGHandler={this.props.addEEGHandler}
+                    secondsToShow={1} 
+                    refreshRate={60}
+                    channel={this.props.channelName}
+                />
+            </div>
+            <div className="column"> 
+                <PSD options={this.props.options} />
+            </div>
+        </div>
         );
     }
-}
+  }
 
-Row.propTypes = {
-    addEEGHandler: PropTypes.func.isRequired,
-    options: PropTypes.object,
-    channelName: PropTypes.string.isRequired
-}
+export default Row;
